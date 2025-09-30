738e5577463dfe02f0f62de123d8df79164495ad
monkeyt170 <301336@shakopeeschools.org>
2025-09-30 09:43:11 -0500

    Update 119_TY.py
    added color changes and size changing for the user.

--- 1.1.9 - Algorithms and Art/119_TY.py
+++ 1.1.9 - Algorithms and Art/119_TY.py
@@ -5,23 +5,35 @@ color6 = trtl.textinput("What color should the 6 be?","Enter color here")
 color7 = trtl.textinput("What color should the 7 be?","Enter color here")
  
 #ask for size
-size6 = trtl.textinput("What size should it be?","Enter size here")
-size7 = trtl.textinput("What size should it be?","Enter size here")
+size6 = trtl.numinput("What size should it be?","Enter size here")
+size7 = trtl.numinput("What size should it be?","Enter size here")
 
 #setup custom turtle
+
+
 # draw the 6
 painter.penup()
-painter.goto(50,0)
+painter.goto(-50,0)
 painter.pendown()
 painter.color(color6)
 painter.setheading(90)
-painter.circle(50,180)
-painter.forward(60)
+painter.circle(size6,180)
+painter.forward(1.4*size6)
+x = painter.xcor()
+y = painter.ycor()
 painter.penup()
-painter.goto(-50,-70)
+painter.goto(x,y)
 painter.pendown()
-painter.circle(50)
-#draw the 7 
+painter.circle(size6)
+painter.penup()
+#draw the 7
+painter.goto(50,50)
+painter.color(color7)
+painter.pendown()
+painter.setheading(0)
+painter.forward(size7*2)
+painter.setheading(240)
+painter.forward(size7*4)
 
 
 
