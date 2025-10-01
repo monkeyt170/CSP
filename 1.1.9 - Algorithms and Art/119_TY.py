6402a17e8e629f53e1af2ba4a8550e9d5b95f3a2
monkeyt170 <301336@shakopeeschools.org>
2025-10-01 09:02:59 -0500

    Update 119_TY.py
    added a thing to randomly change the colors and have a default size

--- 1.1.9 - Algorithms and Art/119_TY.py
+++ 1.1.9 - Algorithms and Art/119_TY.py
@@ -1,39 +1,55 @@
+
 import turtle as trtl
-painter = trtl.Turtle()
+import random
+
+number = trtl.Turtle()
+
 # ask for colors
-color6 = trtl.textinput("What color should the 6 be?","Enter color here")
-color7 = trtl.textinput("What color should the 7 be?","Enter color here")
- 
+custom_colors = trtl.textinput("Do you want custom colors","Enter yes/no")
+colors = ["red","orange","gold","green","blue","purple"]
+if (custom_colors == "yes"):
+    color6 = trtl.textinput("What color should the 6 be?","Enter color here")
+    color7 = trtl.textinput("What color should the 7 be?","Enter color here")
+else:
+    color6 = random.choice(colors)
+    color7 = random.choice(colors)
+    
 #ask for size
-size6 = trtl.numinput("What size should it be?","Enter size here")
-size7 = trtl.numinput("What size should it be?","Enter size here")
-
+custom_size = trtl.textinput("Do you wanta custom size","Enter yes/no")
+if custom_size == "yes":
+    size6 = trtl.numinput("What size should it be?","Enter size here")
+    size7 = trtl.numinput("What size should it be?","Enter size here")
+else:
+    size6 = 67
+    size7 = 67
 #setup custom turtle
-
+ 
 
 # draw the 6
-painter.penup()
-painter.goto(-50,0)
-painter.pendown()
-painter.color(color6)
-painter.setheading(90)
-painter.circle(size6,180)
-painter.forward(1.4*size6)
-x = painter.xcor()
-y = painter.ycor()
-painter.penup()
-painter.goto(x,y)
-painter.pendown()
-painter.circle(size6)
-painter.penup()
+number.penup()
+number.goto(-50,0)
+number.pendown()
+number.color(color6)
+number.setheading(90)
+number.circle(size6,180)
+number.forward(1.4*size6)
+x = number.xcor()
+y = number.ycor()
+number.penup()
+number.goto(x,y)
+number.pendown()
+number.circle(size6)
+number.penup()
+
 #draw the 7
-painter.goto(50,50)
-painter.color(color7)
-painter.pendown()
-painter.setheading(0)
-painter.forward(size7*2)
-painter.setheading(240)
-painter.forward(size7*4)
+y = 0 + size7
+number.goto(50,y)
+number.color(color7)
+number.pendown()
+number.setheading(0)
+number.forward(size7*2)
+number.setheading(240)
+number.forward(size7*4)
 
 
 
