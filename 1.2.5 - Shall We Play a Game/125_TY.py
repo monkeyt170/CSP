98f27d13ba8345f486da3c0ec6dd7472f27f0974
monkeyt170 <301336@shakopeeschools.org>
2025-10-27 09:21:51 -0500

    Update 125_TY.py
    created file and added milestones

---
+++ 1.2.5 - Shall We Play a Game/125_TY.py
@@ -0,0 +1,30 @@
+#setup
+
+
+#Setting difficulty for how much turtles spawn 
+
+
+
+
+
+#Random spawning turtle on the screen 
+
+
+
+#Turtle moving as an object with arrow keys
+
+
+
+
+#Score of moving the object
+
+
+
+#Turtle hits turtle ends game
+
+
+
+#End game screen with scoreboard
+
+
+
