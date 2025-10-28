1df7c1516db3032f29a1d2b554d8bb8874637709
monkeyt170 <301336@shakopeeschools.org>
2025-10-28 08:24:34 -0500

    Update 125_TY.py
    started moving the turttle

--- 1.2.5 - Shall We Play a Game/125_TY.py
+++ 1.2.5 - Shall We Play a Game/125_TY.py
@@ -1,5 +1,8 @@
 #setup
+import turtle as trtl
+import random as rand
 
+wn = trtl.Screen()
 
 #Setting difficulty for how much turtles spawn 
 
@@ -12,6 +15,16 @@
 
 
 #Turtle moving as an object with arrow keys
+mover = trtl.Turtle()
+
+def move_direction(active_turtle,d):
+    active_turtle.setheading(d)
+    active_turtle.foward(20)
+
+
+
+
+
 
 
 
@@ -28,3 +41,7 @@
 
 
 
+wn = trtl.Screen()
+wn.setup(width=600, height=600)
+wn.cv._rootwindow.resizable(False, False)
+wn.mainloop()
