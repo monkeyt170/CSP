b8f3a4fb51f83285ed1634d39c9b00f806422b33
monkeyt170 <301336@shakopeeschools.org>
2025-11-12 09:04:25 -0600

    inshed the milestone of y/n screen
    finshed the milestone of y/n screen

--- 1.3.1 - Artistic Expression/131_ChilCard.py
+++ 1.3.1 - Artistic Expression/131_ChilCard.py
@@ -7,6 +7,10 @@
 import turtle as trtl
 envolope = trtl.Turtle()
 
+maze1img = "maze1.png"
+wn = trtl.Screen()
+
+
 def envolope_screen():
     envolope.goto(100,100)
     envolope.goto(100,-100)
@@ -18,7 +22,7 @@ def envolope_screen():
     envolope.goto(0,0)
 
 yn = trtl.Turtle()
-yn.hideturtle()
+no = trtl.Turtle()
 def yn_screen(x, y):
     envolope.clear()
     envolope.hideturtle()
@@ -26,11 +30,52 @@ def yn_screen(x, y):
     yn.penup()
     yn.goto(-100,-100)
     yn.write("yes", align="center", font=("Arial", 28, "bold"))
+    no.penup()
+    no.goto(100,-100)
+    no.write("no", align="center", font=("Arial", 28, "bold"))
+
+def maze1(x, y):
+    yn.clear()
+    no.clear()
+    yn.hideturtle()
+    no.hideturtle()
+    movertrtl.penup()
+    movertrtl.goto(-100, 60)
+    movertrtl.setheading(270)
+    wn.bgpic(maze1img)
+
+movertrtl = trtl.Turtle()
+def up():
+    movertrtl.setheading(90)
+    movertrtl.forward(10)
+def down():
+    movertrtl.setheading(270)
+    movertrtl.forward(10)
+def left():
+    movertrtl.setheading(180)
+    movertrtl.forward(10)
+def right():
+    movertrtl.setheading(0)
+    movertrtl.forward(10)
+wn.listen()
+wn.onkey(up, "Up")
+wn.onkey(down, "Down")
+wn.onkey(left, "Left")
+wn.onkey(right, "Right")
+
+def enourage_screen():
+    if movertrtl.pos == (-100,50):
+        wn.clear()
+        envolope.write("test")
+        
+    
 
 
     
 
 envolope.onclick(yn_screen)
+yn.onclick(maze1)
+
 
 
 
@@ -38,5 +83,5 @@ envolope.onclick(yn_screen)
 envolope_screen()
 
 
-wn = trtl.Screen()
+
 wn.mainloop()
